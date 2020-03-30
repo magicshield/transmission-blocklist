40e3f93376ae742bfee5f400b5835d58c8541778
胡春辉 <chhuihu@gmail.com>
2020-03-30 21:24:50 +0800

    fetch blocklist using firefox ua

--- blocklist-build.sh
+++ blocklist-build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -s https://www.iblocklist.com/lists.php | sed -n "s/.*value='\(http.*\)'.*/\1/p" | sed "s/\&amp;/\&/g" | sed "s/http/\"http/g" | sed "s/gz/gz\"/g" | xargs wget -O - |  gunzip | egrep -v '^#' | gzip > blocklist.gz
+curl -s -A "Mozilla/5.0 (X11; Linux x86_64; rv:74.0) Gecko/20100101 Firefox/74.0" https://www.iblocklist.com/lists.php | sed -n "s/.*value='\(http.*\)'.*/\1/p" | sed "s/\&amp;/\&/g" | sed "s/http/\"http/g" | sed "s/gz/gz\"/g" | xargs wget -O - |  gunzip | egrep -v '^#' | gzip > blocklist.gz
